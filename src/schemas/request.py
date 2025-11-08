83739f4ff814893f00c752e09b2811de25f70d3c
大空スバル大好き <111629702+BlueBoy247@users.noreply.github.com>
2025-11-09 02:07:33 +0800

    feat: update request schemas

--- src/schemas/request.py
+++ src/schemas/request.py
@@ -1,5 +1,6 @@
 from uuid import UUID
 from datetime import datetime
+from typing import Optional
 
 from pydantic import BaseModel
 
@@ -8,12 +9,13 @@ from schemas.base import Location
 class ComputeRequest(BaseModel):
     class ClosestPlaceRequestModel(BaseModel):
         user_location: Location
-        sport: str
+        sport: Optional[str] = None
 
 class RecordRequest(BaseModel):
     class CreateRecordRequestModel(BaseModel):
         user_id: UUID
-        place: str
+        place_id: int
         sport: str
         start_time: datetime
         end_time: datetime
+        capacity: int
