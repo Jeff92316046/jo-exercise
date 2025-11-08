4c1197c1c8b80436e0197a4c769515e48fab0a1e
大空スバル大好き <111629702+BlueBoy247@users.noreply.github.com>
2025-11-09 02:07:17 +0800

    feat: record public router

--- src/api/router.py
+++ src/api/router.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 
+from api.record_public import router as record_public_router
 from api.record import router as record_router
 from api.list import router as list_router
 from api.compute import router as compute_router
 
 api_router = APIRouter()
 
+api_router.include_router(record_public_router)
 api_router.include_router(record_router)
 api_router.include_router(list_router)
 api_router.include_router(compute_router)
